72129cbc025e51679542dfdaedc36a5a579e18f0
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2024-04-26 03:14:56 +0900

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig(({ command, mode }) => {
-  const isDevMode = mode === 'development';
+  const isDevMode = mode === "development";
 
   return {
     plugins: [react()],
-    base: isDevMode ? '/' : '/roro/',
+    base: isDevMode ? "/" : "/roro/",
     server: {
       proxy: {
-        '/v1': {
-          target: 'https://maps.googleapis.com',
+        "/v1": {
+          target: "https://maps.googleapis.com",
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/v1/, ''),
+          rewrite: (path) => path.replace(/^\/v1/, ""),
           secure: false,
           ws: true,
         },
